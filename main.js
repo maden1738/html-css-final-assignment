7b318040b63e7810b1c56ad3be36887c6d2a56d9
Swarna Jang Maden <limbujungmaden@gmail.com>
2024-06-04 06:05:36 +0545

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function toggleTheme() {
+     const body = document.querySelector("body");
+
+     if (body.classList.contains("light-mode")) {
+          body.classList.remove("light-mode");
+          body.classList.add("dark-mode");
+     } else {
+          body.classList.remove("dark-mode");
+          body.classList.add("light-mode");
+     }
+}
